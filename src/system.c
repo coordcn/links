e049b31a8aa687cf2afc85f9943ae15f6fdfb8c9
coordcn <coordcn@163.com>
2015-03-23 13:56:45 +0800

    system

---
+++ src/system.c
@@ -0,0 +1,154 @@
+/**************************************************************
+  @copyright: Copyright(c) 2015 coord.cn All rights reserved
+  @author: QianYe(coordcn@163.com)
+  @license: MIT license
+  @overview: 
+ **************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+# include <sys/utsname.h>
+
+#include "system.h"
+
+static int links_system_cpuinfo(lua_State* L){
+  uv_cpu_info_t* cpu_infos;
+  int count, i, index;
+
+  if(uv_cpu_info(&cpu_infos, &count)) return luaL_error(L, "system.cpuinfo() error.");
+
+  lua_createtable(L, count, 0);
+  for(i = 0; i < count; i++){
+    uv_cpu_info_t* ci = cpu_infos + i;
+    index = i + 1;
+
+    lua_createtable(L, 0, 3);
+    lua_pushlstring(L, ci->model, strlen(ci->model));
+    lua_setfield(L, -2, "model");
+    lua_pushinteger(L, ci->speed);
+    lua_setfield(L, -2, "speed");
+
+    lua_createtable(L, 0, 5);
+    lua_pushinteger(L, ci->cpu_times.user);
+    lua_setfield(L, -2, "user");
+    lua_pushinteger(L, ci->cpu_times.nice);
+    lua_setfield(L, -2, "nice");
+    lua_pushinteger(L, ci->cpu_times.sys);
+    lua_setfield(L, -2, "sys");
+    lua_pushinteger(L, ci->cpu_times.idle);
+    lua_setfield(L, -2, "idle");
+    lua_pushinteger(L, ci->cpu_times.irq);
+    lua_setfield(L, -2, "irq");
+
+    lua_setfield(L, -2, "times");
+
+    lua_rawseti(L, -2, index);
+  }
+
+  uv_free_cpu_info(cpu_infos, count);
+
+  return 1;
+}
+
+static int links_system_meminfo(lua_State* L){
+  uint64_t total = uv_get_total_memory();
+  uint64_t free = uv_get_free_memory();
+  
+  lua_createtable(L, 0, 2);
+
+  lua_pushinteger(L, total);
+  lua_setfield(L, -2, "total");
+
+  lua_pushinteger(L, free);
+  lua_setfield(L, -2, "free");
+
+  return 1;
+}
+
+static int links_system_loadavg(lua_State* L){
+  double loadavg[3];
+  uv_loadavg(loadavg);
+  
+  lua_createtable(L, 3, 0);
+
+  lua_pushnumber(L, loadavg[0]);
+  lua_rawseti(L, -2, 1);
+  lua_pushnumber(L, loadavg[1]);
+  lua_rawseti(L, -2, 2);
+  lua_pushnumber(L, loadavg[2]);
+  lua_rawseti(L, -2, 3);
+
+  return 1;
+}
+
+/*nanoseconds*/
+static int links_system_hrtime(lua_State* L){
+  uint64_t hrtime = uv_hrtime();
+  lua_pushinteger(L, hrtime);
+
+  return 1;
+}
+
+/*seconds*/
+static int links_system_uptime(lua_State* L){
+  double uptime;
+  if(uv_uptime(&uptime)) return luaL_error(L, "system.uptime() error.");
+  
+  lua_pushnumber(L, uptime);
+
+  return 1;
+}
+
+static int links_cannot_change(lua_State* L){
+  return luaL_error(L, "table fields cannot be changed.");
+}
+
+int luaopen_system(lua_State *L){
+  const char* type;
+  const char* release;
+  const char* endian;
+
+  struct utsname info;
+  if(uname(&info) < 0){
+    type = "Unknown";
+    release = "Unknown";
+  }
+
+  type = info.sysname;
+  release = info.release;
+
+  union {
+    uint32_t i;
+    uint8_t c;
+  } u;
+  u.i = 1;
+  endian = (u.c == 1) ? "LE" : "BE";
+
+  luaL_Reg lib[] = {
+    { "cpuinfo", links_system_cpuinfo},
+    { "meminfo", links_system_meminfo},
+    { "loadavg", links_system_loadavg},
+    { "hrtime", links_system_hrtime},
+    { "uptime", links_system_uptime},
+    { "__newindex", links_cannot_change},
+    { NULL, NULL}
+  };
+
+  lua_createtable(L, 0, 0);
+
+  luaL_newlib(L, lib);
+  lua_pushlstring(L, type, strlen(type));
+  lua_setfield(L, -2, "type");
+  lua_pushlstring(L, release, strlen(release));
+  lua_setfield(L, -2, "release");
+  lua_pushlstring(L, endian, 2);
+  lua_setfield(L, -2, "endian");
+  lua_pushvalue(L, -1);
+  lua_setfield(L, -2, "__index");
+  lua_pushliteral(L, "metatable is protected.");
+  lua_setfield(L, -2, "__metatable");
+
+  lua_setmetatable(L, -2);
+
+  return 1;
+}
---
+++ src/system.h
@@ -0,0 +1,18 @@
+/**************************************************************
+  @copyright: Copyright(c) 2015 coord.cn All rights reserved
+  @author: QianYe(coordcn@163.com)
+  @license: MIT license
+  @overview: 
+ **************************************************************/
+
+#ifndef LINKS_SYSTEM_H
+#define LINKS_SYSTEM_H
+
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+#include "uv.h"
+
+int luaopen_system(lua_State *L);
+
+#endif /*LINKS_SYSTEM_H*/
